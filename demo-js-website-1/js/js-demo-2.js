2fa9a33a84757d0d569c21a54179c58827842aad
adavorn <jacob46king@gmail.com>
2025-02-17 14:55:25 -0600

    edit js page

---
+++ demo-js-website-1/js/js-demo-2.js
@@ -0,0 +1,17 @@
+        //javascript single line comment
+
+        /*
+        javascript multi
+        line comment
+        */
+        
+        //group code in a name called function
+        //define function
+        function display_info_1()
+        {
+            console.log("# Js Demo 2")
+            //alert (# JS Demo 1)
+        }
+
+        //call function
+        display_info_1();

7a4130833f3b5b69d3cd20a203331ced022e25c6
adavorn <jacob46king@gmail.com>
2025-02-18 11:54:06 -0600

    added to js page 1

--- demo-js-website-2/js/page1.js
+++ demo-js-website-2/js/page1.js
@@ -1,5 +1,27 @@
 //javascript single line comment
 
 /*
-javascript multi line comment
+javascript multi line 
+comment
 */
+
+function display_info(){
+    console.log("# Page1 - Display Info")
+}
+
+function page_load(){
+    console.log("# Page1 - Page Load")
+    display_info()
+}
+
+function demo_link1() {
+    console.log("# Page1 - Link1")
+    alert("# Page1 - Link1")
+}
+
+function demo_button1()
+{
+    console.log("# Page1 - Button1")
+    alert("# Page1 - Button1")
+}
+

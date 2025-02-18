9f398826c1d999a1e15f89bfeab82a45d3ea1f72
adavorn <jacob46king@gmail.com>
2025-02-18 16:54:15 -0600

    added to js page 1

--- demo-js-website-2/js/page1.js
+++ demo-js-website-2/js/page1.js
@@ -270,5 +270,16 @@ function demo_variables_numbers(){
 }
 
 function demo_variables_strings(){
-    
+    const _function_name = "page1: demo_variables_strings";
+
+    let str1 = "intro to js"
+    const str2 = " javascript "
+    const _version = "es6+"
+
+    console.log(_function_name.toUpperCase)
+    console.log("length: ", _function_name.length)
+    console.log(str1)
+    console.log(str2)
+    console.log(str2.trim())
+
 }

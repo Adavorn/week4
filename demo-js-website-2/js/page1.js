34e348d9971229cc5bce89c76041750e0ba14cb6
adavorn <jacob46king@gmail.com>
2025-02-18 14:01:20 -0600

    added to js page 1

--- demo-js-website-2/js/page1.js
+++ demo-js-website-2/js/page1.js
@@ -127,6 +127,110 @@ function demo_variables2(){
 }
 
 function demo_variables3(){
+
     console.log("# Page1 - variables3")
     //alert("# Page1 - variables3")
+
+    //variable - in memory data, to uses later in your code
+    //data type - type of data stored in memory -  string, number, boolean, true/false, objects, other
+
+    //define memory varrible to be used later in code
+    //global variables - accessed by other functions
+    //global or page level variables -defined outside a function ,accessed by any function
+    //function variables - access only by current function
+    
+    //let - javascript es5+ 2009+
+    //let - define a local/function in memory variable
+    //let - reccomended: more optimized memory variable
+    let var1 = "intro to javascript"; //data-type - string
+    let var_2 = "6"; //data-type - string "text inside double quotes counts as string"
+    let var2 = 6 //data-type - number integer
+    let var3 = 6.1 //data-type - number floating point including decimal
+    let var4 = true //data-type boolean - true/false
+
+    //other data types - boolean = true/false, object
+
+    //can convert string to number
+
+    //when function end - memory variable is removed from memory
+
+    //use variable
+    console.log(var1)
+
+    //combine string variables = concatination
+    console.log(var1 + var_2)
+
+    //combine string and number = concatination
+    console.log(var1 + var2)
+
+    //combine string and number = concatination
+    console.log(var1 + " " + var2)
+
+    //combine variables = concatination
+    console.log(var1,var2,var3)
+    
+    //const - readonly, not to be updated
+    //varibles const can be updated but not reccomended to update
+    //update variable
+    //javascript does not generate an error if const are updated
+    var3 = 6.3 //data-type = float, can update to any other data type
+
+    //combine variables = concatination
+    console.log(var1,var2,var3)
+
+}
+
+function demo_variables4(){
+
+    console.log("# Page1 - variables4")
+    //alert("# Page1 - variables4")
+
+    //variable - in memory data, to uses later in your code
+    //data type - type of data stored in memory -  string, number, boolean, true/false, objects, other
+
+    //define memory varrible to be used later in code
+    //global variables - accessed by other functions
+    //global or page level variables -defined outside a function ,accessed by any function
+    //function variables - access only by current function
+    
+    //const - javascript es5+ 2009+
+    //const - define a local/function in memory variable that should not be updated when defined
+    //const - reccomended: define and not update const variavle later in code
+    //const = fixed and readonly when assigned
+    const var1 = "intro to javascript"; //data-type - string
+    const var_2 = "6"; //data-type - string "text inside double quotes counts as string"
+    const var2 = 6 //data-type - number integer
+    const var3 = 6.1 //data-type - number floating point including decimal
+    const var4 = true //data-type boolean - true/false
+
+    //other data types - boolean = true/false, object
+
+    //can convert string to number
+
+    //when function end - memory variable is removed from memory
+
+    //use variable
+    console.log(var1)
+
+    //combine string variables = concatination
+    console.log(var1 + var_2)
+
+    //combine string and number = concatination
+    console.log(var1 + var2)
+
+    //combine string and number = concatination
+    console.log(var1 + " " + var2)
+
+    //combine variables = concatination
+    console.log(var1,var2,var3)
+    
+    //const - read only, not to be updated
+    //varibles const can be updated but not reccomended to update
+    //update variable
+    //javascript does not generate an error if const are updated
+    var3 = 6.3 //data-type = float, can update to any other data type
+
+    //combine variables = concatination
+    console.log(var1,var2,var3)
+
 }

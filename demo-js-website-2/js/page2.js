4ecc8b43e08e366bd3d8cd601328b7ff21949d16
adavorn <jacob46king@gmail.com>
2025-02-19 13:40:23 -0600

    eddited pg 2

--- demo-js-website-2/js/page2.js
+++ demo-js-website-2/js/page2.js
@@ -23,3 +23,33 @@ function lnk_dom_demo1()
     //document.getElementById("div-display-info").innerText = msgtext;
     document.getElementById("div-display-info").innerHTML = msgtext;
 }
+
+function lnk_dom_demo2()
+{
+    //Js DOM- select an html element in the page the manipulate element: update element text
+
+    let msgtext = "<b># Intro to Js DOM #2</b>" //data type - string
+
+    console.log(msgtext);
+
+    //#data type object HTMLelement object - has different structer then string,boolean, number
+    //# learn more about different object types ie: w3schools or online refrences
+    //# ie: HTMLElement object -- different structers
+    //# Json object -- different structers
+    const eleDivInfo = document.getElementById("div-display-info"); //data type = HTMLelement or null
+ 
+    //# if statement - make a decsion in a code - ture/false
+    //# if evaluates two values, if true call true code else if false call false code
+    //# if (logical expression) = if a == b or if != bor if a > b etc..
+    if(eleDivInfo == null){
+        //true code
+        //# div-display-info not found ie: null
+        console.log(" #div-display-info not found")
+    }
+    else
+    {
+        //false code
+        //#div-display-info not found ie: null
+        eleDivInfo.innerText = msgtext;
+    }
+}

30f68f21eedfa7f8ec40c5402ed570dd51bca739
adavorn <jacob46king@gmail.com>
2025-02-20 12:34:33 -0600

    eddited pg 2

--- demo-js-website-2/js/page2.js
+++ demo-js-website-2/js/page2.js
@@ -104,3 +104,72 @@ function lnk_dom_demo3()
     //clear atext
     txtsearch.value = "";
 }
+
+function btnSearch(){
+
+    let msgText = "# page2: btnSearch" //string 
+    console.log(msgText.toUpperCase());  
+
+    //select text field and display div using document.getElementById
+    const txtsearch = document.getElementById("txtsearch"); //HTMLElement or null
+    const divdisplayinfo = document.getElementById("div-display-info"); //HTMLElement or null
+
+    //validate if text field and display div is null, if so and exit function
+    if(txtsearch == null){
+        msgText = "# txtsearch field not found"
+        console.log(msgText)
+        return false //stop onclick and exit function 
+    }
+
+    if(divdisplayinfo == null){
+        msgText = "# divdisplayinfo field not found"
+        console.log(msgText)
+        return false //stop onclick and exit function 
+    }
+
+    //######## other if condition examples ####
+    //if(txtsearch != null) -> not null 
+    //if(txtsearch == null ! divdisplayinfo == null)  -> or 
+    //if(txtsearch == null & divdisplayinfo == null) -> or
+
+    //validate if text field is empty, if so exit function
+    if(txtsearch.value.trim().length == 0) {
+        msgText = "# please enter a valid search text"
+        console.log(msgText)
+        divdisplayinfo.innerText = msgText
+        return false //stop onclick and exit function 
+    }
+
+    //display search text in display div
+    msgText = "# searching for: " + txtsearch.value
+
+    //divdisplayinfo.innerHTML = "<br>" + msgText + "</b>"
+    divdisplayinfo.innerText = msgText;
+
+    txtsearch.value = ""; //clear text field
+    txtsearch.focus(); //move keyboard cursor to text field
+
+}
+
+function lnkClear(){
+    
+    let msgText = "# page2: lnkClear"
+    console.log(msgText.toUpperCase());
+
+    //select text field and display div using document.getElementById
+    const txtsearch = document.getElementById("txtsearch")
+    const divdisplayinfo = document.getElementById("div-display-info")
+
+    //validate if text field and display div is null, if so and exit function
+    //-- undefined = field was not defined or made
+    //-- null - field was definded and does not have a value
+    //undefined variable is avariable that was not created
+
+    if(txtsearch == null | txtsearch == undefined) {
+
+        msgText = "# txtsearch is null"
+        return false
+    }
+    //clear text field
+    //set keyboard focus to the text field
+}

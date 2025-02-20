d03d5656f2330765ed07c4925f676cf79d3e6382
adavorn <jacob46king@gmail.com>
2025-02-20 17:09:44 -0600

    edited contact page

--- demo-js-website-2/js/contact.js
+++ demo-js-website-2/js/contact.js
@@ -1,17 +1,82 @@
 function page_load(){
     const msgText = "# page3: page load";
+
     console.log(msgText.toUpperCase())
 }
 
 
 function btnSubmit(){
-    const msgText = "# page3: btnSubmit";
+
+    let msgText = "# page3: btnSubmit";
+
     console.log(msgText.toUpperCase())
+  
+
+    /*
+        div-display-info:
+            1- select div-display-info using document.getElementById
+            2- validate if div-display-info is not null or undefined, if so, display element not found message and exit function
+    */
+    const divdisplayinfo = document.getElementById("divdisplayinfo");
+
+    /*
+        firtname:
+            1- select firstname field using document.getElementById
+            2- validate if firstname field is not null or undefined, if so, display field not found message and exit function
+            3- validate if firstname field is empty, if so, display invalid field entry message and exit function
+    */
+
+    /*
+        lastname:
+            1- select lastname field using document.getElementById
+            2- validate if lastname field is not null or undefined, if so, display invalid field entry message and exit function
+    */
+
+    /*
+        email:
+            1- select email field using document.getElementById
+            2- validate if email field is not null or undefined, if so, display field not found message and exit function
+            3- validate if email field is empty, if so, display invalid field entry message and exit function
+    */
+
+    /*
+        comment:
+            1- select comment field using document.getElementById
+            2- validate if comment field is not null or undefined, if so, display field not found message and exit function
+            3- validate if comment field is empty, if so, display invalid field entry message and exit function
+    */
+
+    /*
+        //- display form field information in log ie: 
+
+            msgText = "form submitted: firstname: " + txtfirstname.value + ", lastname: " + 
+            txtlastname.value + ", email: " + txtemail.value + ", comment: ";
+
+            console.log(msgText)
+    */
+
+    //divdisplayinfo.innerText = "form submitted"
+
+    /*
+        //clear each field value 
+        txtfirstname.value = "";
+        txtlastname.value = "";
+        txtemail.value = "";
+        txtcomment.value = "";
+
+        txtfirstname.focus();
+    */
+
+    //prevent form post
+    return false
+
 }
 
 
 function btnClear(){
     const msgText = "# page3: btnClear";
+
     console.log(msgText.toUpperCase())
+
 }
 

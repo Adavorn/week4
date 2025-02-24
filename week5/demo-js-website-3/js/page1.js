498a31fe9d01113ce5af6c39c2adbab2e51ced18
adavorn <jacob46king@gmail.com>
2025-02-24 12:09:50 -0600

    added page one js

--- week5/demo-js-website-3/js/page1.js
+++ week5/demo-js-website-3/js/page1.js
@@ -5,9 +5,18 @@ function page_load(){
 
 }
 
+function lnkNonArrayDemo1(){
+    
+    let msgText = "# page1: lnk Non Array Demo1"
+
+    console.log(msgText.toUpperCase())
+
+}
+
+//function name should not be js keyword
 function lnkArrayDemo1(){
     
-    let msgText = "# page1: lnkArrayDemo1"
+    let msgText = "# page1: lnk Array Demo1"
 
     console.log(msgText.toUpperCase())
 
